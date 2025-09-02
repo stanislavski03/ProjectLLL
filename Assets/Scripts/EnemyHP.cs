88ff45b8e17af902c45722b66be253914facf2c6
Prolesk <62287238+Prolesk@users.noreply.github.com>
2025-09-02 21:00:50 +0300

    Создал скрипты на HP

---
+++ Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+
+    [SerializeField] private float _maxHP = 100;
+
+    [SerializeField] private float _freezeDef = 100;
+    [SerializeField] private float _fireDef = 100;
+    [SerializeField] private float _electroDef = 100;
+
+    private float _currentHP;
+
+
+    private void OnEnable()
+    {
+        _currentHP = _maxHP;
+    }
+
+    public void Damage(float damageAmmount, int damageType)
+    {
+        switch(damageType){
+            case 0: 
+                _currentHP -= damageAmmount * _freezeDef / 100;
+                break;
+            case 1:
+                _currentHP -= damageAmmount * _fireDef / 100;
+                break;
+            case 2:
+                _currentHP -= damageAmmount * _electroDef / 100;
+                break;
+            default: _currentHP -= damageAmmount;
+                break;
+        }
+        _currentHP -= damageAmmount;
+        if (_currentHP <= 0 ) Death();
+    }
+
+
+    public void Damage(float damageAmmount)
+    {
+        _currentHP -= damageAmmount;
+        if (_currentHP <= 0) Death();
+    }
+
+    public void Heal(float healAmmount)
+    {
+        _currentHP += healAmmount;
+    }
+
+    private void Death()
+    {
+        Debug.Log("HOLY, HE'S DEAD!");
+        Destroy(gameObject);
+    }
+}
