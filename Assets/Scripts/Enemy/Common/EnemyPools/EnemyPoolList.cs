33edb508ca9e9258ba487d950d01acf31ddd3a97
Prolesk <62287238+Prolesk@users.noreply.github.com>
2025-12-13 15:34:12 +0300

    EnemyNowReturnToPoolWhenPlayerTeleport

--- Assets/Scripts/Enemy/Common/EnemyPools/EnemyPoolList.cs
+++ Assets/Scripts/Enemy/Common/EnemyPools/EnemyPoolList.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class EnemyPoolList : MonoBehaviour
 {
@@ -9,6 +10,15 @@ public class EnemyPoolList : MonoBehaviour
     public EnemyPool _smallRangedEnemy;
     public EnemyPool _fastMeleeEnemy;
     public EnemyPool _strongMeleeEnemy;
+
+    public void EnquePools()
+    {
+        _smallMeleeEnemy.EnquePool();
+        _smallRangedEnemy.EnquePool();
+        _fastMeleeEnemy.EnquePool();
+        _strongMeleeEnemy.EnquePool();
+    }
+
     private void Awake()
     {
         instance = this;
